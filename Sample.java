1201df2a73fc27af0f968dfa39d5b495efb60fe4
Akash Chaudhary <akash.chaudhary@codenation.co.in>
2017-08-13 00:09:16 +0400

    sample java

---
+++ Sample.java
@@ -0,0 +1,27 @@
+public class Sample{
+
+
+public Sample(){}
+
+public static void main(String []args){
+
+switch(1){
+case 1: System.out.println("Hello\n");
+break;
+case 2: System.out.println("World\n");
+break;
+case 3: System.out.println("Hi");
+break;
+default:
+System.out.println("YY");
+
+}
+
+
+}
+
+
+
+
+
+}
