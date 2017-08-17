8a9b89937caabe1e516feae3ab6120d9bb9f7e6b
Akash Chaudhary <akash.chaudhary@codenation.co.in>
2017-08-17 20:19:36 +0400

    a

--- Sample.java
+++ Sample.java
@@ -5,7 +5,8 @@ public Sample(){}
 
 public static void main(String []args){
 
-for(int i=0, j=0, k=0;i<1&&j<=9;i++,j++,k++){
+int i,j,k;
+for( i=0, j=0, k=0;i<1&&j<=9;i++,j++,k++){
 System.out.println("Hello World");
 }
 
