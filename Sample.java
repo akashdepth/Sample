a80f177a58dbdffd98e14ec44d7fc7a705a809b0
Akash Chaudhary <akash.chaudhary@codenation.co.in>
2017-08-21 18:14:10 +0400

    a

--- Sample.java
+++ Sample.java
@@ -1,19 +1,7 @@
 public class Sample{
 
-
-public Sample(){}
-
-public static void main(String []args){
-
-int i,j,k;
-for( i=0, j=0, k=0;i<1&&j<=9;i++,j++,k++){
-System.out.println("Hello World");
+public static void main(String[] args){
+	int[] a={1,2};
 }
-
-}
-
-
-
-
 
 }
